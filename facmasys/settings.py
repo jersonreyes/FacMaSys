0e9c72dbed8c548e84cdab556d3420c001ba3269
Jerson Reyes <jerson.reyes.a@bulsu.edu.ph>
2022-12-29 01:03:54 +0800

    update: settings

--- facmasys/settings.py
+++ facmasys/settings.py
@@ -35,7 +35,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'feed.apps.DashboardConfig',
+    'apps.feed.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
