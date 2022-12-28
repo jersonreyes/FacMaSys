6c9e81e90941a9445cef986128efc2b9232eb5a9
MightyDarkGlasses <cantoria.alexis.p.5055@gmail.com>
2022-12-28 10:29:17 +0800

    Added: Faculty Member side

---
+++ faculty_member/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(ExtensionService_Collaborators)
+admin.site.register(ExtensionService_OfferedPrograms)
+admin.site.register(ExtensionService)
+
+admin.site.register(Research_Presented)
+admin.site.register(Research_Published)
+admin.site.register(Research)
+
+admin.site.register(Subjects_Taught)
+admin.site.register(Subjects)
+
+admin.site.register(Users)
