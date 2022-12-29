2810a24fef9d27c1dd1294bf5d0e152c19f24222
Jerson Reyes <jerson.reyes.a@bulsu.edu.ph>
2022-12-29 12:25:40 +0800

    update: forms

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -10,7 +10,7 @@ class LoginForm(AuthenticationForm):
                                required=True,
                                label="Username or Email",
                                widget=forms.TextInput(attrs={'placeholder': 'Username or Email',
-                                                             'class': 'form-control',
+                                                             'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500',
                                                              }))
     password = forms.CharField(max_length=50,
                                required=True,
