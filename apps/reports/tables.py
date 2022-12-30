4cf7b3eadd0acdaee6a389f1729f839771693b31
Jerson Reyes <jerson.reyes.a@bulsu.edu.ph>
2022-12-31 01:15:30 +0800

    update: reports

--- apps/reports/tables.py
+++ apps/reports/tables.py
@@ -1,9 +1,10 @@
 import django_tables2 as tables
-# from .models import ActivityLog
+
+from .models import ActivityLog
 
 
-# class ActivityTable(tables.Table):
-#     user = tables.Column(accessor='user__username')
-#     class Meta:
-#         model = ActivityLog
-#         template_name = "partials/bootstrap_htmx_table.html"
+class ActivityTable(tables.Table):
+     user = tables.Column(accessor='user__username')
+     class Meta:
+         model = ActivityLog
+         template_name = "partials/bootstrap_htmx_table.html"
--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path('analytics/pdf',views.export_graphs_pdf, name='export-graph-pdf'),
     path('',views.view_reports.as_view(), name='reports_view'),
+    path('log',views.ActivityView.as_view(), name='activity_logs'),
     # path('settings/activitylog',views.ActivityView.as_view(), name='settings'),
     path('notif/delete/<int:pk>/',views.delete_notification, name='notif-delete'),
     path('notif/delete/all',views.delete_all_notification, name='notif-delete-all'),
