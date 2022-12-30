f5ba37d34489ad04b836e0ed65e3b48ae8c58952
HunterX405 <43195082+HunterX405@users.noreply.github.com>
2022-12-30 23:17:17 +0800

    -Activity Log

--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
 from .models import ActivityLog
+from facmasys.utils import add_activity, render_to_pdf
+import pandas as pd
 
-admin.site.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = ('datetime','type','location','user','message')
+    list_filter = ('type','location')
+    search_fields = ('user','type','location')
+    actions = ('export_pdf',)
+    
+    @admin.action(description='Export as PDF')
+    def export_pdf(self, request, queryset):
+        df = pd.DataFrame.from_records(queryset.values('datetime','type','location','user','message'))
+        df.columns = [col.replace('_',' ').title() for col in df.columns]
+        context = {
+            'title':'Activity Log Report',
+            'user':request.user.username,
+            'table':df.to_html(classes=['table', 'table-striped', 'bg-white', 'text-center'],index=False)
+        }
+        add_activity(logged_user=request.user,activity_type='EXPORT PDF',activity_location='ACTIVITY LOG',activity_message=f"Activity_Log_Report.pdf was exported")
+        
+        return render_to_pdf(request,'reports/report.html', f'Activity_Log_Report.pdf', context)
+    
+    
+admin.site.register(ActivityLog, ActivityLogAdmin)
